00a1c14494e6b7e3b67479f2d45ed0a13abe983c
오동현 <dhgus44@gmail.com>
2026-02-09 21:09:41 +0900

    datasource추가

--- src/main/java/monitoring/monitoringdashboard/MonitoringDashBoardApplication.java
+++ src/main/java/monitoring/monitoringdashboard/MonitoringDashBoardApplication.java
@@ -5,7 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EnableScheduling
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class
+})
 public class MonitoringDashBoardApplication {
 
   public static void main(String[] args) {
